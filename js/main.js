c0f28e20dcce40cda040ab9d1f53269ecc2ce92f
Santiago David <sdrrock@gmail.com>
2016-02-27 22:23:55 -0800

    trying to get header

--- js/main.js
+++ js/main.js
@@ -1,20 +1,120 @@
-// Dean Attali / Beautiful Jekyll 2015
+var main = {
 
-// Shorten the navbar after scrolling a little bit down
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar").addClass("top-nav-short");
-    } else {
-        $(".navbar").removeClass("top-nav-short");
+  bigImgEl : null,
+  numImgs : null,
+
+  init : function() {
+    // Shorten the navbar after scrolling a little bit down
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar").addClass("top-nav-short");
+        } else {
+            $(".navbar").removeClass("top-nav-short");
+        }
+    });
+    
+    // On mobile, hide the avatar when expanding the navbar menu
+    $('#main-navbar').on('show.bs.collapse', function () {
+      $(".navbar").addClass("top-nav-expanded");
+    })
+    $('#main-navbar').on('hidden.bs.collapse', function () {
+      $(".navbar").removeClass("top-nav-expanded");
+    })
+
+    // show a message if there is one to show
+    qs = main.getQueryParams();
+    if (qs.message) {
+      $(".container")[0].innerHTML =
+        '<div class="row"><div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">' +
+          '<div class="alert alert-success" role="alert">' +
+            qs.message +
+        "</div></div></div>" +
+        $(".container")[0].innerHTML;
     }
-});
 
-// On mobile, hide the avatar when expanding the navbar menu
-$('#main-navbar').on('show.bs.collapse', function () {
-  $(".navbar").addClass("top-nav-expanded");
-})
-$('#main-navbar').on('hidden.bs.collapse', function () {
-  $(".navbar").removeClass("top-nav-expanded");
-})
+    // show the big header image	
+    main.initImgs();
+  },
+  
+  initImgs : function() {
+	// If the page was large images to randomly select from, choose an image
+    if ($("#header-big-imgs").length > 0) {
+      main.bigImgEl = $("#header-big-imgs");
+      main.numImgs = main.bigImgEl.attr("data-num-img");
 
-// 2fc73a3a967e97599c9763d05e564189
+	  // set an initial image
+	  var imgInfo = main.getImgInfo();
+	  var src = imgInfo.src;
+	  var desc = imgInfo.desc;
+  	  main.setImg(src, desc);
+  	
+	  // For better UX, prefetch the next image so that it will already be loaded when we want to show it
+  	  var getNextImg = function() {
+	    var imgInfo = main.getImgInfo();
+	    var src = imgInfo.src;
+	    var desc = imgInfo.desc;		  
+	    
+		var prefetchImg = new Image();
+  		prefetchImg.src = src;
+		// if I want to do something once the image is ready: `prefetchImg.onload = function(){}`
+		
+  		setTimeout(function(){
+          var img = $("<div></div>").addClass("big-img-transition").css("background-image", 'url(' + src + ')');
+  		  $(".intro-header.big-img").prepend(img);
+  		  setTimeout(function(){ img.css("opacity", "1"); }, 50);
+		  
+		  // after the animation of fading in the new image is done, prefetch the next one
+  		  //img.one("transitioned webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+		  setTimeout(function() {
+		    main.setImg(src, desc);
+			img.remove();
+  			getNextImg();
+		  }, 1000); 
+  		  //});		
+  		}, 6000);
+  	  };
+	  
+	  // If there are multiple images, cycle through them
+	  if (main.numImgs > 1) {
+  	    getNextImg();
+	  }
+    }
+  },
+  
+  getImgInfo : function() {
+  	var randNum = Math.floor((Math.random() * main.numImgs) + 1);
+    var src = main.bigImgEl.attr("data-img-src-" + randNum);
+	var desc = main.bigImgEl.attr("data-img-desc-" + randNum);
+	
+	return {
+	  src : src,
+	  desc : desc
+	}
+  },
+  
+  setImg : function(src, desc) {
+	$(".intro-header.big-img").css("background-image", 'url(' + src + ')');
+	if (typeof desc !== typeof undefined && desc !== false) {
+	  $(".img-desc").text(desc).show();
+	} else {
+	  $(".img-desc").hide();  
+	}
+  },
+ 
+ // get the GET parameters in the URL
+ getQueryParams : function() {
+    qs = document.location.search.split("+").join(" ");
+
+    var params = {}, tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])]
+            = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+  }  
+};
+
+document.addEventListener('DOMContentLoaded', main.init);
