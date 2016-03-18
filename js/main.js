b2809cdf810293d5a7c6402c047f41fb2087ec49
Dean Attali <daattali@gmail.com>
2016-03-17 20:46:26 -0700

    support navbar menus

--- js/main.js
+++ js/main.js
@@ -10,7 +10,19 @@ $(window).scroll(function() {
 // On mobile, hide the avatar when expanding the navbar menu
 $('#main-navbar').on('show.bs.collapse', function () {
   $(".navbar").addClass("top-nav-expanded");
-})
+});
 $('#main-navbar').on('hidden.bs.collapse', function () {
   $(".navbar").removeClass("top-nav-expanded");
-})
+});
+
+// On mobile, when clicking on a multi-level navbar menu, show the child links
+$('#main-navbar').on("click", ".navlinks-parent", function(e) {
+  var target = e.target;
+  $.each($(".navlinks-parent"), function(key, value) {
+	if (value == target) {
+	  $(value).parent().toggleClass("show-children");
+	} else {
+	  $(value).parent().removeClass("show-children");
+	}
+  });
+});	
