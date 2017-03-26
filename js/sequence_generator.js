fc0691fdd53defcc87241e39079812733f6ebd96
Christopher Dinh <cqdinh153@gmail.com>
2017-03-26 03:18:18 -0400

    Update sequence_generator.js

--- js/sequence_generator.js
+++ js/sequence_generator.js
@@ -1,1 +1,10 @@
+alert("abcdef");
 
+function sample(){
+  var values = {};
+  $.each($('#myForm').serializeArray(), function(i, field) {
+    values[field.name] = field.value;
+  });
+  alert("abcasdfasdf");
+  alert(values);
+}
