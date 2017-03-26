e550fc4de7530a56f4cb53a6c97504ee9a395f10
Christopher Dinh <cqdinh153@gmail.com>
2017-03-26 03:35:55 -0400

    Update sequence_generator.js

--- js/sequence_generator.js
+++ js/sequence_generator.js
@@ -1,10 +1,15 @@
 function sample(){
-    console.log('Form submitted!');
+    var sample_type_select = document.getElementById("sample_type");
+    var sample_type = sample_type_select.options[sample_type_select.selectedIndex].value;
+    var sample_size = parseInt(document.getElementById("sample_size").value);
+    var sample_seed = document.getElementById("sample_seed").value;
+    console.log(sample_type);
+    console.log(sample_size);
+    console.log(sample_seed);
 }
 
 function init(){
     document.getElementById('sample-form').onsubmit = sample;
-    console.log("init");
 }
 
 init();
