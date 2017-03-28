3c3cf24a661eccebd95f27880ae5520123991a9b
Christopher Dinh <cqdinh153@gmail.com>
2017-03-28 13:11:44 -0400

    Update models.js

--- js/models.js
+++ js/models.js
@@ -6,3 +6,35 @@ var decoders = [];
 var hidden_sizes = []
 var state_sizes = [];
 var depths = [];
+
+weights["character"] = [
+  [
+    [],
+    []
+  ],
+  [
+    [],
+    []
+  ]
+];
+biases["character"] = [
+  [],
+  []
+  ];
+embeddings["character"] = [
+  [],
+  []
+  ];
+encoders["character"] = [
+  
+  ];
+decoders["character"] = [
+  
+  ];
+hidden_sizes["character"] = [
+  
+  ];
+state_sizes["character"] = [
+  
+  ];
+depths["character"] = 2;
