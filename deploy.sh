5c261e48637e8d09650738531535fae9220ffd88
harryhcm <holala.tiger@gmail.com>
2019-09-18 19:15:49 +0700

    Vblog công nghệ

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+# Add changes to git.
+
+git status
+
+git add .
+
+# Commit changes.
+
+git commit -m "Vblog công nghệ"
+
+
+# Push source and build repos.
+git push origin master --force
+
+git pull origin master --force
+
+
+# check the status
+
+git status
+
+git commit -m "Tech cuong cong nghe"
