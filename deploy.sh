328aeb5ee2284666e3494ba518658f4700879990
holalatiger <holala.tiger@gmail.com>
2019-09-19 08:46:09 +0700

    Vblog công nghệ

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+# Add changes to git.
+
+git status
+
+git add .
+
+# Commit changes.
+
+git commit -m "Vblog công nghệ"
+
+
+# Push source and build repos.
+git push origin master 
+
+
+# check the status
+
+git status
+
+git commit -m "Tech cuong cong nghe"
