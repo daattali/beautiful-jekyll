e86d5b6cee5c028ea71aaa398701deaf539edfd9
Eyal Golan <eyalgolan96@gmail.com>
2019-10-30 17:14:37 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', 'js/particles.json', function() {
+particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
