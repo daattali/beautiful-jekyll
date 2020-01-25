333a61f362bd6e23855b4020a3552cfc4b676246
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-01-25 19:03:37 +0100

    clean

--- doc/0.9.1/nightly/search/enumvalues_f.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['rgbmode',['RGBMode',['../classDGtal_1_1Viewer3D.html#a542350413b6cfa5ffae5cd6b21a81750ac16ceeaaa1538b8865c4f83ef73f84e0',1,'DGtal::Viewer3D']]],
-  ['rice',['Rice',['../classDGtal_1_1Histogram.html#a6ad9233127d51070df46f56534a6aa6ea7bdacc70c2bdd53b2a9f9e2295e92be3',1,'DGtal::Histogram']]],
-  ['right_5fvalue_5fref',['RIGHT_VALUE_REF',['../classDGtal_1_1Alias.html#a22a7083f7b9350124620c5d49e329b08a72333d922685156e4c98795e2e03e6a9',1,'DGtal::Alias::RIGHT_VALUE_REF()'],['../classDGtal_1_1Clone.html#ad6256b7bcf50412643a5d485da330b76a694724cd43b607c01c39716f2c44d66e',1,'DGtal::Clone::RIGHT_VALUE_REF()'],['../classDGtal_1_1ConstAlias.html#a5fb4ec0c751d31c22b68fa8af4f86fcfa5111b0138d6efb569cec9a36e1787981',1,'DGtal::ConstAlias::RIGHT_VALUE_REF()']]],
-  ['roundcap',['RoundCap',['../structLibBoard_1_1Shape.html#af09a3c6f719c8a846447c0cd2e2d0368ad2b5f9290405771e387b70d62ae0c194',1,'LibBoard::Shape']]],
-  ['roundjoin',['RoundJoin',['../structLibBoard_1_1Shape.html#a5618d9c1f412d36189695c557f1b9be2abff2d9f2c1448ab19f2276a143700f71',1,'LibBoard::Shape']]]
-];
--- doc/0.9.1/nightly/search/namespaces_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['hull2d',['Hull2D',['../namespaceHull2D.html',1,'']]]
-];
