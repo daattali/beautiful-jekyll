15cd15d7fda95ee7614ac6deec31f310741ab543
Nghia Truong <thongtincovid19.n@gmail.com>
2020-04-18 18:52:08 +0900

    Patient by prefectures

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -1,10 +1,64 @@
 import datetime
+import json
+import urllib.request
 import sys
 
 import pandas as pd
 
+
+PREFECTURES = {
+    '北海道': 'Hokkaido',
+    '青森県': 'Aomori',
+    '岩手県': 'Iwate',
+    '宮城県': 'Miyagi',
+    '秋田県': 'Akita',
+    '山形県': 'Yamagata',
+    '福島県': 'Fukushima',
+    '茨城県': 'Ibaraki',
+    '栃木県': 'Tochigi',
+    '群馬県': 'Gunma',
+    '埼玉県': 'Saitama',
+    '千葉県': 'Chiba',
+    '東京都': 'Tokyo',
+    '神奈川県': 'Kanagawa',
+    '新潟県': 'Niigata',
+    '富山県': 'Toyama',
+    '石川県': 'Ishikawa',
+    '福井県': 'Fukui',
+    '山梨県': 'Yamanashi',
+    '長野県': 'Nagano',
+    '岐阜県': 'Gifu',
+    '静岡県': 'Shizuoka',
+    '愛知県': 'Aichi',
+    '三重県': 'Mie',
+    '滋賀県': 'Shiga',
+    '京都県': 'Kyoto',
+    '大阪県': 'Osaka',
+    '兵庫県': 'Hyogo',
+    '奈良県': 'Nara',
+    '和歌山県': 'Wakayama',
+    '鳥取県': 'Tottori',
+    '島根県': 'Shimane',
+    '岡山県': 'Okayama',
+    '広島県': 'Hiroshima',
+    '山口県': 'Yamaguchi',
+    '徳島県': 'Tokushima',
+    '香川県': 'Kagawa',
+    '愛媛県': 'Ehime',
+    '高知県': 'Kochi',
+    '福岡県': 'Fukuoka',
+    '佐賀県': 'Saga',
+    '長崎県': 'Nagasaki',
+    '熊本県': 'Kumamoto',
+    '大分県': 'Oita',
+    '宮崎県': 'Miyazaki',
+    '鹿児島県': 'Kagoshima',
+    '沖縄県': 'Okinawa',
+}
+
+
 def get_and_cleanse_tokyo_data(auto_drop: bool = False) -> pd.DataFrame:
-    """Get data from Toyko Metropolitan Government and localize to Vietnamese.
+    """Get data from Tokyo Metropolitan Government and localize to Vietnamese.
     
     Args:
         auto_drop (bool): If True, drop all columns with less than 2 unique values.
@@ -68,9 +122,9 @@ def get_and_cleanse_tokyo_data(auto_drop: bool = False) -> pd.DataFrame:
         '湖北省武漢市': 'Vũ Hán, Hồ Bắc',
         '湖南省長沙市': 'Trường Sa, Hồ Nam',
         '都内': 'Nội đô Tokyo',
-        '埼玉県': 'Saitama',
         '都外': 'Ngoài Tokyo',
         '調査中': 'Đang điều tra',
+        **PREFECTURES,
     }, inplace=True)
     tokyo_data[COL_PATIENT_SEX].replace({
         '男性': 'Nam',
@@ -106,13 +160,28 @@ def get_and_cleanse_tokyo_data(auto_drop: bool = False) -> pd.DataFrame:
     return tokyo_data
 
 
+def get_and_cleanse_prefecture_data() -> pd.DataFrame:
+    """Get data from NHK and localize to Vietnamese."""
+    request = urllib.request.Request(
+        'https://www3.nhk.or.jp/news/special/coronavirus/data/input-pref.json',
+        headers={'User-Agent': 'Mozilla/5.0'}
+    )
+    with urllib.request.urlopen(request) as url:
+        data = json.loads(url.read().decode())
+
+    df = pd.DataFrame(data['prefList'])
+    df['pref'].replace(PREFECTURES, inplace=True)
+    return data['lastupdate'], df
+
+
 def main(args=None):
     tokyo_data = get_and_cleanse_tokyo_data()
+    _, patients_by_prefecture = get_and_cleanse_prefecture_data()
     
     now = datetime.datetime.now()
     timestamp = now.strftime('%Y%m%d_%H%M')
-    file_names = ['tokyo']
-    for file_name, data in zip(file_names, [tokyo_data]):
+    file_names = ['tokyo', 'all_prefectures']
+    for file_name, data in zip(file_names, [tokyo_data, patients_by_prefecture]):
         data.to_csv(f'{timestamp}_{file_name}.csv', index=False)
     
     return 0
