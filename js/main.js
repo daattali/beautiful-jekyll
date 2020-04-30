240599b860b989ce3ebb090d52d9640298195d5b
heocoi <dev.hibiki@gmail.com>
2020-05-01 00:25:44 +0900

    add kanagawa data

--- js/main.js
+++ js/main.js
@@ -946,3 +946,78 @@ async function renderSaitamaByWardGraph() {
     console.error(error);
   }
 }
+
+async function renderKanagawaByWardGraph() {
+  try {
+    const storage = firebase.storage();
+    // daily graph
+    if ($("#graph-byward").length > 0) {
+      // load data from firebase storage
+      const fileRef = storage.ref("patient-by-city-kanagawa.json");
+      const url = await fileRef.getDownloadURL().catch((e) => {
+        throw e;
+      });
+      const response = await fetch(url).catch((e) => {
+        throw e;
+      });
+      const responseData = await response.json().catch((e) => {
+        throw e;
+      });
+
+      const data = _.chain(responseData)
+        .groupBy("Location")
+        .mapValues((v, k) => {
+          return { location: k, count: v.length };
+        })
+        .values()
+        .sortBy("count")
+        .reverse()
+        .value();
+
+      Highcharts.chart("graph-byward", {
+        chart: {
+          type: "column",
+        },
+        legend: {
+          enabled: false,
+        },
+        title: {
+          text: `Số bệnh nhân theo quận`,
+        },
+        subtitle: {
+          text: `Nguồn: <a href="http://www.pref.kanagawa.jp/osirase/1369/">Chính quyền thành phố Kanagawa</a>`,
+        },
+        xAxis: {
+          categories: _.map(data, "location"),
+          min: 0,
+          max: 10,
+          scrollbar: {
+            enabled: true,
+            liveRedraw: true,
+          },
+        },
+        yAxis: {
+          min: 0,
+          max: _.max(_.map(data, "count")),
+          title: false,
+        },
+        plotOptions: {
+          column: {
+            pointPadding: 0.2,
+            borderWidth: 0,
+          },
+        },
+        series: [
+          {
+            name: "Số bệnh nhân",
+            data: _.map(data, "count"),
+            color: "rgba(255,159,64,1)",
+          },
+        ],
+      });
+    }
+  } catch (error) {
+    // TODO: error handler
+    console.error(error);
+  }
+}
