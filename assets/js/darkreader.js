8cbb2838a1826c50b54f40846daab3eb5e360ce7
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2020-09-27 20:36:55 +1000

    Add css sheet for darkmode (hiding icons), update js

--- assets/js/darkreader.js
+++ assets/js/darkreader.js
@@ -1,7 +1,38 @@
 // Enable when system color scheme is dark
-DarkReader.auto({
-    brightness: 100,
-    contrast: 100,
-    sepia: 0
-});
 
+$(function() {
+  var root = $(":root")
+  const dr_settings = {
+        brightness: 100,
+        contrast: 100,
+        sepia: 0
+  };
+
+  if (
+    localStorage.getItem('color-mode') === 'light' ||
+    (window.matchMedia('prefers-color-scheme: light').matches &&
+    !localStorage.getItem('color-mode'))) {
+
+    root.setAttribute("color-mode", "light");
+  }
+
+  if (!root.getAttribute("color-mode") === 'light') {
+    DarkReader.enable(dr_settings);
+  }  
+
+  $("js-dark-toggle").on("click", (e) => {
+    var root = $(":root");
+    var mode = root.getAttribute("color-mode");
+
+    if (mode === "light") {
+      root.setAttribute("color-mode", "dark");
+      localStorage.setItem("color-mode", "dark");
+      DarkReader.disable();
+    } else {
+      root.setAttribute("color-mode", "light");
+      localStorage.setItem("color-mode", "light");
+      DarkReader.enable(dr_settings);
+    }
+  
+  } );
+})
