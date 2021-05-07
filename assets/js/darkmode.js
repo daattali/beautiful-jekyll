6f67278e228fed2a713cd743017e4dd84678ab51
Raihan Khan <raihankhanraka@gmail.com>
2021-05-07 17:45:24 +0600

    Update darkmode.js

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -4,7 +4,7 @@ const options = {
   left: '32px', // default: 'unset'
   time: '0.5s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff',  // default: '#fff'
+  backgroundColor: '#85929E',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: false, // default: true,
