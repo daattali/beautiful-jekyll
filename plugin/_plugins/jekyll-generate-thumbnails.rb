515b904da7cf2a43d64c69a7d8365bf69382079f
Andrew G <55633473+geande@users.noreply.github.com>
2021-12-11 04:33:50 -0500

    Delete plugin directory

--- plugin/_plugins/jekyll-generate-thumbnails.rb
+++
@@ -1,3 +0,0 @@
-#Remove the '#' to enable automatic thumbnail generation with image magick.
-#See https://github.com/zroger/jekyll-minimagick for information on how to set it up on your system.
-#require "jekyll-minimagick"
--- plugin/images/photography/thumbnails/README.md
+++
@@ -1,4 +0,0 @@
-#README
-
-These thumbnails can be auto-generated with imageMagick (see the main README file for a tutorial) or you can just export them
-from Lightroom/Photoshop etc. in the right size.
