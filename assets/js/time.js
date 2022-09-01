6eb69981c0c05858486eb647a8675e0ce0b3fc2e
liusxs <1694181697@qq.com>
2022-09-01 17:30:15 +0800

    Update time.js

--- assets/js/time.js
+++ assets/js/time.js
@@ -20,6 +20,6 @@ function siteTime() {
   var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
   var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes);
   var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes * minutes) / seconds);
-  document.getElementById("sitetime").innerHTML = "liuliu_blog已经运行了：" + diffDays + " 天 " + diffHours + " 小时 " + diffMinutes + " 分钟 " + diffSeconds + " 秒";
+  document.getElementById("sitetime").innerHTML = "柳柳的宝藏已经运行了：" + diffDays + " 天 " + diffHours + " 小时 " + diffMinutes + " 分钟 " + diffSeconds + " 秒";
 }
 siteTime();
