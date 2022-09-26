1b8315309f089d853ad7ce82033af89109bcef18
BCD03 <93735194+BCD03@users.noreply.github.com>
2022-09-26 13:09:18 +0530

    Create button.css

---
+++ assets/css/button.css
@@ -0,0 +1,10 @@
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
