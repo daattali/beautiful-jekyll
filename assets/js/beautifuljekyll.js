fcc56263cfad27a08bc6bbe6d5a45e53fa9647b1
Workday Brains <123598600+workdaybrains@users.noreply.github.com>
2023-01-26 23:49:47 -0500

    fix

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -140,5 +140,3 @@ var BeautifulJekyllJS = {
 // 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', BeautifulJekyllJS.init);
-document.querySelector('#main-navbar > ul > li.nav-item.dropdown').innerHTML='';
-document.querySelector('#main-navbar > ul > li:nth-child(3) > a').innerHTML='';
