5a5d65b09bb6c249337a1720249e346cf9d60263
fleurgaudfernau <60889188+fleurgaudfernau@users.noreply.github.com>
2023-06-27 20:09:41 +0200

    Update and rename controller.html to controller.js

---
+++ controller.js
@@ -0,0 +1,3 @@
+const controlPanel = () =>{
+"<html><table> <tr>  <td> <label for='timeslider'>Time step:</label> <input id='timeslider' type='range' min='0' max='1' step='1' /> </td> </tr> <tr> <td> <p>Time value: <span id='timevalue'>DOWNLOADING...</span></p> </td> </tr></table></html>"
+};
