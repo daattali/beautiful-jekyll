45a6be21d2c3f4124678c3edd17b5c6e8fafdd95
Jade Therras <jade.therras@epfl.ch>
2024-12-10 13:01:13 +0100

    trying some stuff for the sidebar and homepage

---
+++ assets/js/sidebar.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
